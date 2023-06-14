0a393ed7fa2799fd9047174cedcee94c94bb9c23
Zhuo <hudirybw@gmail.com>
2023-06-14 13:58:49 +0200

    not working

--- src/components/game_main.jsx
+++ src/components/game_main.jsx
@@ -4,12 +4,17 @@ import {Context} from "../context/Context"
 function GameMain(){
     const {newgame} = useContext(Context)
 
-    return (<div className="container ">
-        {newgame.map( x => (
-            <div>{x}</div>
-        ))}
+    return (<div className="container">
+        {newgame.map((x,i) =>{
+        if (i % 9 === 0 ) return <div className="row"><div key={i} className="m-2 p-2 col">{x}</div>
+        else if (i % 9 === 8 ) return <div key={i} className="m-2 p-2 col">{x}</div></div>
+        return <div key={i} className="m-2 p-2 col">{x}</div>
+    })}
+      
 
     </div>)
 }
 
 export default GameMain;
+
+
